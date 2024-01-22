235e95bd72fc685430438e6d35fc6b3928004d87
brorby <magnus.sbr12@gmail.com>
2024-01-22 12:36:37 +0100

    small fixes with css

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -4,13 +4,14 @@ import NavigationComponent from "./NavigationComponent";
 import "../App.css";
 
 function App() {
-
   return (
     <div>
       {/* {alertVisible && <Alert children={"Shamboo"} onClick={() => setAlertVisibility(false)}></Alert>}
       <MyBtn color="danger" children={"my Button"} onClick={() => setAlertVisibility(true)} /> */}
       {/* <ChatComponent></ChatComponent> */}
-      <div className="chatComponent"><ChatComponent></ChatComponent></div>
+      <div className="chatComponent">
+        <ChatComponent></ChatComponent>
+      </div>
       <div className="navigation">
         <NavigationComponent />
       </div>
--- src/Components/NavigationComponent.tsx
+++ src/Components/NavigationComponent.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import Data from "./Data";
-import "../Navigation.css";
 
 let data = "";
 
